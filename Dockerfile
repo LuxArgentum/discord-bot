2df9460232b91586b20c2d59b366b6cec4777178
MatthewSong <matthewsong18@gmail.com>
2024-03-28 22:22:51 -0700

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.9-slim
+
+WORKDIR /app
+
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
+
+COPY bot.py ./
+
+EXPOSE 8000
+
+CMD ["python", "bot.py"]
--- bot.py
+++ bot.py
@@ -16,18 +16,19 @@ bot.auto_sync_commands = True
 
 @bot.event
 async def on_ready():
-    channel = bot.get_channel(1212927556911767645)
+    channel = bot.get_channel(int(os.environ["BOT_CHANNEL"]))
     await channel.send('Bot is now running!\n'
                        f'Current Time: {discord.utils.utcnow()}')
     await bot.sync_commands()
 
 
-@bot.slash_command(name='hello', description='Say hello!', guild_ids=[1212927555330383912])
+@bot.slash_command(name='hello', description='Say hello!', guild_ids=[int(os.environ["SERVER_ID"])])
 async def hello(command_context):
+    # TODO: Change code from author_name to pinging the user that called the command.
     await command_context.respond(f'Hello, {command_context.author.name}!', ephemeral=True)
 
 
-@bot.slash_command(name='announcement', description='Make an announcement!', guild_ids=[1212927555330383912])
+@bot.slash_command(name='announcement', description='Make an announcement!', guild_ids=[int(os.environ["SERVER_ID"])])
 async def announcement(command_context, announcement_message: discord.Option(str, "Enter your announcement")):
     await command_context.respond(f'@everyone\n'
                                   f'\n'
